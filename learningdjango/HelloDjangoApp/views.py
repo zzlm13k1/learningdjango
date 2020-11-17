8e142fb5f6069141bd8c028ed8a6b94fc5408f91
zzlm13k1 <zzlm13k1@libero.it>
2020-11-17 07:32:30 +0100

    seconda modifica

--- learningdjango/HelloDjangoApp/views.py
+++ learningdjango/HelloDjangoApp/views.py
@@ -2,6 +2,20 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+# def index(request):
+#   return HttpResponse("Hello, Django!")
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
--- learningdjango/learningdjango/settings.py
+++ learningdjango/learningdjango/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
+    'HelloDjangoApp',
     # Add your apps here to enable them
     'django.contrib.admin',
     'django.contrib.auth',
